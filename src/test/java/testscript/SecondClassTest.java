bf99a6f7f9646b8a0a0159ea9b50fd1bfb355bc7
unknown <suneel.kumar.uttam@gmail.com>
2025-06-28 18:44:42 +0530

    adding schedule

---
+++ src/test/java/testscript/SecondClassTest.java
@@ -0,0 +1,13 @@
+package testscript;
+
+import org.testng.annotations.Test;
+
+public class SecondClassTest {
+	
+	@Test
+	public void mySecondMethod()
+	{
+		System.out.println("Hello GitHub- Second Class");
+	}
+
+}
