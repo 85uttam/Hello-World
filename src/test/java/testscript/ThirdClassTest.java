d4e58f3809fbb84a3b936defe4bedf66353b796c
Suneel Uttam <suneel.kumar.uttam@gmail.com>
2025-06-29 21:23:39 +0530

    This is Thirs Class
    This is Third and driver Class

---
+++ src/test/java/testscript/ThirdClassTest.java
@@ -0,0 +1,13 @@
+package testscript;
+
+import org.testng.annotations.Test;
+
+public class ThirdClassTest {
+	
+	@Test
+	public void mySecondMethod()
+	{
+		System.out.println("Hello GitHub- Third Class");
+	}
+
+}
